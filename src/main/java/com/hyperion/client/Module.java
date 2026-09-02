7c145e955f54b85e61a3369a444f2725552b8bf4
longswordsmp <tomasdeicaza@icloud.com>
2026-09-01 21:30:06 -0400

    Implement abstract Module class with basic functionality

--- src/main/java/com/hyperion/client/Module.java
+++ src/main/java/com/hyperion/client/Module.java
@@ -1,1 +1,17 @@
-
+package com.hyperion.client;
+import net.minecraft.client.MinecraftClient;
+public abstract class Module {
+    protected MinecraftClient mc = MinecraftClient.getInstance();
+    private boolean enabled = false;
+    private String name;
+    private ModuleCategory category;
+    public Module(String name, ModuleCategory category) { this.name = name; this.category = category; }
+    public void toggle() { enabled = !enabled; if (enabled) onEnable(); else onDisable(); }
+    public boolean isEnabled() { return enabled; }
+    public void onEnable() {}
+    public void onDisable() {}
+    public void onTick() {}
+    public void onRender() {}
+    public String getName() { return name; }
+    public ModuleCategory getCategory() { return category; }
+}
