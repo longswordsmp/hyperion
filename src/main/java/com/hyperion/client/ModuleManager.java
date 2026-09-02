d9b973a08dc207003cd15bcf51a7f67675646303
longswordsmp <tomasdeicaza@icloud.com>
2026-09-01 21:31:05 -0400

    Create ModuleManager to register and manage modules
    Added ModuleManager class to manage modules.

--- src/main/java/com/hyperion/client/ModuleManager.java
+++ src/main/java/com/hyperion/client/ModuleManager.java
@@ -1,1 +1,13 @@
-
+package com.hyperion.client;
+import java.util.ArrayList;
+import java.util.List;
+public class ModuleManager {
+    private static List<Module> modules = new ArrayList<>();
+    public static void registerAll() {
+        modules.add(new com.hyperion.modules.basefinding.BaseFinder());
+        modules.add(new com.hyperion.modules.basefinding.SusChunkFinder());
+        modules.add(new com.hyperion.modules.visual.ESP());
+        // add other modules here
+    }
+    public static void onTick() { for (Module m : modules) if (m.isEnabled()) m.onTick(); }
+}
